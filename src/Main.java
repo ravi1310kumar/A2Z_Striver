f047085155c1c944ce64b5c3edaa2fadc0ebe1f3
Ravi Kumar <ravik67596@gmail.com>
2026-01-25 07:18:23 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" I am a java code and form today onward i will learn dsa and practice here.");
+        System.out.println("Hi Everyone");
     }
 }
